d8db2a4734d9cc4171b2689de8553fbd8f30b029
장시영 <seeyoung@wholedoc.net>
2024-06-21 10:22:24 +0900

    chatbot sample

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import (Flask, redirect, render_template, request,
-                   send_from_directory, url_for)
+                   send_from_directory, url_for, jsonify)
 
 app = Flask(__name__)
 
@@ -27,6 +27,39 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+
+@app.route('/sayHello', methods=['GET'])
+def say_hello():
+    response_body = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(response_body)
+
+@app.route('/showHello', methods=['GET'])
+def show_hello():
+    response_body = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleImage": {
+                        "imageUrl": "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
+                        "altText": "hello I'm Ryan"
+                    }
+                }
+            ]
+        }
+    }
+    return jsonify(response_body)
 
 if __name__ == '__main__':
    app.run()
